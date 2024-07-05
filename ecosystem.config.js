0f864eb0c0584a338d3ad68d0e1cd004a0d8e237
ali <alishams.s1380@gmail.com>
2024-07-05 18:30:24 +0330

    f

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+      {
+        name: 'shop',
+        exec_mode: 'cluster',
+        instances: 'max', // Or a number of instances
+        script: './.output/server/index.mjs',
+        port: 3000,
+        args: 'start'
+      }
+    ]
+  }
